425e11c1e88fa487cb30d061309edf947c1f913a
Ferry Huberts <ferry.huberts@pelagic.nl>
2013-03-24 15:57:47 -0400

    lab1: basic DS

--- org.eclipsecon.masterosgi.orderentry/src/org/eclipsecon/masterosgi/orderentry/OrderEntry.java
+++ org.eclipsecon.masterosgi.orderentry/src/org/eclipsecon/masterosgi/orderentry/OrderEntry.java
@@ -1,5 +1,8 @@
 package org.eclipsecon.masterosgi.orderentry;
 
+import java.util.Collection;
+
+import org.example.exchange.api.Exchange;
 import org.example.model.orders.Orders;
 import org.example.model.orders.Orders.Order;
 import org.example.model.orders.Orders.Order.Side;
@@ -7,6 +10,7 @@ import org.example.model.orders.Orders.Order.Side;
 import aQute.bnd.annotation.component.Activate;
 import aQute.bnd.annotation.component.Component;
 import aQute.bnd.annotation.component.Deactivate;
+import aQute.bnd.annotation.component.Reference;
 
 @Component(
 	provide = Object.class,
@@ -17,21 +21,31 @@ import aQute.bnd.annotation.component.Deactivate;
 	}
 )
 public class OrderEntry {
+	
+	private Exchange exchange;
+
+	@Reference
+	public void setExchange(Exchange exchange) {
+		this.exchange = exchange;
+	}
 
 	public void orders() {
 		System.out.println("Listing orders in the exchange:");
-		// TODO: implement this command to list the current orders in the exchange
+		Collection<Order> orders = exchange.getAllOrders();
+		System.out.printf("There are %d orders in the exchange:%n", orders.size());
+		for (Order order : orders) {
+			System.out.println(order);
+		}
 	}
 	
-	public void bid(String symbol, long quantity, long price) {
+	public void bid(String symbol, long quantity, long price) throws Exception {
 		Order newOrder = Orders.Order.newBuilder()
 			.setSide(Side.BID)
 			.setSymbol(symbol)
 			.setQuantity(quantity)
 			.setUnitPrice(price)
 			.build();
-		
-		// TODO: submit this order into the exchange
+		exchange.submitOrder(newOrder);
 	}
 
 	@Activate
